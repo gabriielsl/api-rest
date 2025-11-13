de8423a53006d9701f960494dd548ad48c273c54
Gabriel Lima <gabriiel_sl@hotmail.com>
2025-11-12 22:24:48 -0300

    Criando a primeira migration

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,16 @@
-import knex from "knex";
+import knex from "knex";            // Importação da Lib
+import type { Knex } from "knex"    // Importação apenas do tipo
 
-export const setupKnex = knex({
-    client: "sqlite",
+export const config: Knex.Config = {
+    client: "sqlite3",
     connection: {
-        filename: "./tmp/app.db",
+        filename: "./db/app.db",
     },
-})
+    useNullAsDefault: true,
+    migrations: {
+        extension: "ts",
+        directory: "./db/migrations"
+    }
+}
+
+export const setupKnex = knex(config)
