691bd28d4cc90b71a9c1797acd132e2446e7357d
Gabriel Lima <gabriiel_sl@hotmail.com>
2025-11-19 22:08:59 -0300

    Criando a rota de transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,12 +1,26 @@
+import { z } from "zod"
 import type { FastifyInstance } from "fastify"
 import { setupKnex } from "../database.js"
+import { randomUUID } from "node:crypto"
 
-export function transactionsRoutes(app: FastifyInstance) {
-    app.post("/", async () => {
-        const transaction = await setupKnex("transactions")
-        .select("*")
-        .where("amount", 500)
-    
-        return transaction
+export async function transactionsRoutes(app: FastifyInstance) {
+    app.post("/", async (request, reply) => {
+        const createTransactionBodySchema = z.object({
+            text: z.string(),
+            amount: z.number(),
+            type: z.enum(["credit", "debit"])
+        })
+
+        const { text, amount, type } = createTransactionBodySchema.parse(
+            request.body
+        )
+
+        await setupKnex("transactions").insert({
+            id: randomUUID(),
+            text,
+            amount: type === "credit" ? amount : amount * -1 
+        })
+
+        return reply.status(201).send()
     })
 }
