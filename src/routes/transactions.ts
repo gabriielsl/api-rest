5ab5e92f02c24b0cc1fb978564dfb84e594a9199
Gabriel Lima <gabriiel_sl@hotmail.com>
2025-12-01 18:54:13 -0300

    Rotas de listagem de transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,27 @@ import { setupKnex } from "../database.js"
 import { randomUUID } from "node:crypto"
 
 export async function transactionsRoutes(app: FastifyInstance) {
+    // Rota de Listagem das transações
+    app.get("/", async () => {
+        const transactions = await setupKnex("transactions").select()
+
+        return { transactions }
+    })
+
+    // Rota de listagem única
+    app.get("/:id", async (request) => {
+        const getTransactionParamsSchema = z.object({
+            id: z.string().uuid()
+        })
+
+        const { id } = getTransactionParamsSchema.parse(request.params)
+
+        const transaction = await setupKnex("transactions").where("id", id).first()
+
+        return { transaction }
+    })
+
+    // Rota de criação de transações
     app.post("/", async (request, reply) => {
         const createTransactionBodySchema = z.object({
             text: z.string(),
@@ -11,6 +32,7 @@ export async function transactionsRoutes(app: FastifyInstance) {
             type: z.enum(["credit", "debit"])
         })
 
+        // Validação dos tipos vindos da requisição, comparando-os ao schema
         const { text, amount, type } = createTransactionBodySchema.parse(
             request.body
         )
@@ -18,7 +40,7 @@ export async function transactionsRoutes(app: FastifyInstance) {
         await setupKnex("transactions").insert({
             id: randomUUID(),
             text,
-            amount: type === "credit" ? amount : amount * -1 
+            amount: type === "credit" ? amount : amount * -1,
         })
 
         return reply.status(201).send()
