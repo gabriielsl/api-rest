13bc1fb6c59d971792111355b82803cf0aab2960
Gabriel Lima <gabriiel_sl@hotmail.com>
2025-12-05 18:29:04 -0300

    Inserção de cookies

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import fastify from "fastify"
+import cookie from "@fastify/cookie"
+
 import { setupKnex } from "./database.js"
 import { env } from "./env/index.js"
 import { transactionsRoutes } from "./routes/transactions.js"
 
 const app = fastify()
 
+app.register(cookie)
+
+// Prefixo de todas as rotas da aplicação
 app.register(transactionsRoutes, {
     prefix: "transactions"
 })
